4724f7b05b3b8124a376756bddbaa419bdf2461e
Karla Sai Krishna Reddy <karla.s@thoughtworks.com>
2022-07-12 10:20:53 +0530

    adds. current direction method

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,17 +1,21 @@
 import coordinates.Coordinates;
 
-import java.util.Objects;
-
 public class Rover {
 
     private final Coordinates coordinates;
+    char direction;
 
-    public Rover(Coordinates initialCoordinates) {
+    public Rover(Coordinates initialCoordinates, char initialDirection) {
 
         coordinates = initialCoordinates;
+        direction = initialDirection;
     }
 
     public Coordinates currentLocation() {
         return this.coordinates;
+    }
+
+    public char currentDirection() {
+        return this.direction;
     }
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -7,11 +7,22 @@ public class RoverTest {
     @Test
     void shouldProvideCurrentCoordinates() {
         Coordinates initialCoordinates = new Coordinates(0, 0);
-        Rover rover = new Rover(initialCoordinates);
+        Rover rover = new Rover(initialCoordinates, 'N');
         Coordinates expectedCoordinates = new Coordinates(0, 0);
 
         Coordinates actualCoordinates = rover.currentLocation();
 
         assertEquals(actualCoordinates, expectedCoordinates);
+    }
+
+    @Test
+    void shouldProvideCurrentDirection() {
+        Coordinates initialCoordinates = new Coordinates(0, 0);
+        char initialDirection = 'N';
+        Rover rover = new Rover(initialCoordinates, initialDirection);
+
+        char actualDirection = rover.currentDirection();
+
+        assertEquals(initialDirection, actualDirection);
     }
 }
